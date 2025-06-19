8cbc8a404e54a31dc9751cd705795751e0aeedac
Partonetrain <Partonetrain@gmail.com>
2025-06-19 18:18:00 -0500

    script is functional

--- MalumSpiritDataGen.py
+++ MalumSpiritDataGen.py
@@ -2,19 +2,34 @@ import os
 import json
 import time
 import csv
+from dataclasses import dataclass
 
 ENTRIES_FILE_NAME = "entries.csv"
 TEMPLATE_FILE_NAME = "template.json"
 SPIRIT_DIR = "malum/spirit_data/entity"
-items = [[],[],[],[],[],[],[]] #Number of sublists = number of tiers
+entries = [] 
 start_time = 0
 
+@dataclass
+#constructor generated automatically
+class Entry:
+    modid: str
+    mobname: str
+    primaryType: str
+    sacredSpirits: int
+    wickedSpirits: int
+    arcaneSpirits: int
+    eldritchSpirits: int
+    aerialSpirits: int
+    aqueousSpirits: int
+    earthenSpirits: int
+    infernalSpirits: int
 
 def main():
   print('MalumSpiritDataGen by Partonetrain')
   start_time = time.perf_counter()
   checkFilesExist()
-  ParseItems()
+  ParseEntries()
   recipes_generated = FillTemplate()
   end_time = time.perf_counter()
   execution_time = end_time - start_time
@@ -27,17 +42,34 @@ def checkFilesExist():
         time.sleep(3)
         quit()
 
-def ParseItems():
+def ParseEntries():
   with open(ENTRIES_FILE_NAME, 'r') as csv_file:
-    reader = csv.reader(csv_file, skipinitialspace=True)
+    reader = csv.DictReader(csv_file, skipinitialspace=True)
+    #dictreader allows addressing with str
 
     item_count = 0
     line = 1 #ignore header
     for row in reader:
-      for column in row:
-        items[line].append(column)
-        item_count = item_count + 1
-      line = line + 1
+        if(not "#" in row["mobId"]):
+          split = row["mobId"].split(":")
+          entry = Entry(
+                    modid=split[0],
+                    mobname=split[1],
+                    primaryType=row["primaryType"],
+                    sacredSpirits=int(row["sacredSpirits"]),
+                    wickedSpirits=int(row["wickedSpirits"]),
+                    arcaneSpirits=int(row["arcaneSpirits"]),
+                    eldritchSpirits=int(row["eldritchSpirits"]),
+                    aerialSpirits=int(row["aerialSpirits"]),
+                    aqueousSpirits=int(row["aqueousSpirits"]),
+                    earthenSpirits=int(row["earthenSpirits"]),
+                    infernalSpirits=int(row["infernalSpirits"]),
+                )
+          entries.append(entry)
+          line = line + 1
+          item_count = item_count + 1
+          #print(entry)
+          
     print(f"parsed {item_count} entries")
     csv_file.close()
 
@@ -50,27 +82,52 @@ def FillTemplate():
     template_data = json.load(json_file)
 
   tryMakeDir(SPIRIT_DIR)
-  for row in items:
-    for item_id in row:
-      split_id = item_id.split(":")
-      mod_id = split_id[0]
-      print("Mod ID: " + mod_id)
-      item_name = split_id[1]
-      path = SPIRIT_DIR + "\\" + mod_id
-      tryMakeDir(path)
-      recipe_data = template_data
+  for entry in entries:
+    recipe_data = ""
+    #print("filling template for " + str(entry))
+    path = SPIRIT_DIR + "\\" + entry.modid
+    tryMakeDir(path)
+    recipe_data = template_data
 
-      recipe_data["ingredient"]["item"] = item_id
+    recipe_data["registry_name"] = entry.modid + ":" + entry.mobname
 
-      recipe_data["repairTime"] = getTimeFromTier(tier)
-        
-      recipe_path = path + "\\" + item_name + ".json"
-      with open(recipe_path, "w") as output_file:
+    recipe_data["primary_type"] = entry.primaryType
+    recipe_data["spirits"].clear()
+    #I don't know why but without the clear it retains the
+    #spirits of the previous entry
+
+    if(entry.sacredSpirits > 0):
+        print(f"{entry.modid}:{entry.mobname} has {entry.sacredSpirits} sacred")
+        recipe_data["spirits"].append({"sacred": entry.sacredSpirits})
+    if(entry.wickedSpirits > 0):
+        print(f"{entry.modid}:{entry.mobname} has {entry.wickedSpirits} wicked")
+        recipe_data["spirits"].append({"wicked": entry.wickedSpirits})
+    if(entry.arcaneSpirits > 0):
+        print(f"{entry.modid}:{entry.mobname} has {entry.arcaneSpirits} arcane")
+        recipe_data["spirits"].append({"arcane": entry.arcaneSpirits})
+    if(entry.eldritchSpirits > 0):
+        print(f"{entry.modid}:{entry.mobname} has {entry.eldritchSpirits} eldritch")
+        recipe_data["spirits"].append({"eldritch": entry.eldritchSpirits})
+    if(entry.aerialSpirits > 0):
+        print(f"{entry.modid}:{entry.mobname} has {entry.aerialSpirits} aerial")
+        recipe_data["spirits"].append({"aerial": entry.aerialSpirits})
+    if(entry.aqueousSpirits > 0):
+        print(f"{entry.modid}:{entry.mobname} has {entry.aqueousSpirits} aqueous")
+        recipe_data["spirits"].append({"aqueous": entry.aqueousSpirits})
+    if(entry.earthenSpirits > 0):
+        print(f"{entry.modid}:{entry.mobname} has {entry.earthenSpirits} earthen")      
+        recipe_data["spirits"].append({"earthen": entry.earthenSpirits})
+    if(entry.infernalSpirits > 0):
+        print(f"{entry.modid}:{entry.mobname} has {entry.infernalSpirits} infernal")      
+        recipe_data["spirits"].append({"infernal": entry.infernalSpirits})
+
+                                      
+    recipe_path = path + "\\" + entry.mobname + ".json"
+    with open(recipe_path, "w") as output_file:
         json.dump(recipe_data, output_file)
-        print("Generated recipe for " + item_id)
+        print("Generated spirit data for " + entry.modid + ":" + entry.mobname)
         recipes_generated = recipes_generated + 1
-      output_file.close()
-    tier = tier + 1
+    output_file.close()
   return recipes_generated
 
 def getTimeFromTier(tier):
@@ -90,13 +147,7 @@ def getTimeFromTier(tier):
         return 3000
 
 def tryMakeDir(path):
-  try:
-    os.mkdir(path)
-    print(f"created '{path}' directory")
-  except FileExistsError:
-    #print(f"did not create '{path}' directory")
-    pass
-
+    os.makedirs(path, exist_ok=True) #mkdirs can create subdirs
 
 if __name__ == '__main__':
   main()
--- README.md
+++ README.md
@@ -1,10 +1,7 @@
-# AetherRepairRecipeGen
+# MalumSpiritDataGen
 
-Generates repair recipes for the Altar (formerly Enchanter). These can be placed in datapacks to enhance compatibility with The Aether.
-[This is necessary since this type of recipe currently does not properly support tags](https://github.com/The-Aether-Team/The-Aether/issues/2546).
-Only tested with The Aether on NeoForge 1.21.1. You may be able to modify the script or template for other versions.
-
+Generates [Malum spirit data](https://github.com/SammySemicolon/Malum-Mod/tree/1.21.1/src/main/resources/data/malum/spirit_data)
 # Usage
-1. Edit items.csv. Each line represents a material tier, which is used to determine the repair time (and thus the amount of fuel necessary). By default, 1 ambrosium shard burns for 250 ticks, and 1 ambrosium block burns for 2500. The script supports up to post-Netherite tier but can be easily modified to support more.
-2. Run AetherRepairRecipeGen.py. It will display what directories and recipe JSONS were created.
-3. Place "recipe" folder in datapack.
+1. Edit entries.csv. 
+2. Run MalumSpiritDataGen.py.
+3. Place "malum" folder in datapack.
